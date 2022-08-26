f854b243c2ef0e1a586a5180f0dc3a5af6d5410c
Gamze Karabulut <gamzekarabuuluut@gmail.com>
2022-08-27 00:04:36 +0300

    Project00 is uploaded.

---
+++ Project00/src/com/godoro/geometry/RectangleTest4.java
@@ -0,0 +1,12 @@
+package com.godoro.geometry;
+
+public class RectangleTest4 {
+
+	public static void main(String[] args) {
+		Rectangle rectangle=new Rectangle();
+		//rectangle.setWidth=-3;
+		//rectangle.setHeight=-4;
+		System.out.println("Alan: "+rectangle.getArea());
+		System.out.println("Çevre1: "+rectangle.getPerimeter());
+	}
+}
