c56c2a5d31e12bd229c32dbcc807a042fc3f6112
Gamze Karabulut <83762365+KarabulutGamze@users.noreply.github.com>
2022-09-13 12:11:55 +0300

    Update Project03

--- Project03/src/com/godoro/database/TestFind.java
+++ Project03/src/com/godoro/database/TestFind.java
@@ -1,5 +1,36 @@
 package com.godoro.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class TestFind {
+	
+	  public static void main(String[] args) throws Exception {
+			
+			String url = "jdbc:postgresql://localhost/postgres";
+			String user = "postgres";
+			String password = "gamze1999";
+			String driver="org.postgresql.Driver";
+			Class.forName(driver);
+			
+			Connection connection = DriverManager.getConnection(url,user,password);
+		
+			String sql="select * from Product where productId=?";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setLong(1, 2);
+			ResultSet resultSet=statement.executeQuery();
+			if(resultSet.next()) {
+				long productId=resultSet.getLong("productId");
+				String productName=resultSet.getString("productName");	
+				double salesPrice=resultSet.getDouble("salesPrice");
+				System.out.println(productId+" "+productName+" "+salesPrice);
+
+			}
+
+			connection.close();
+			
+	  }
 
 }
